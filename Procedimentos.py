173206b49058d26906779ef73a59f67b09f2f78b
gustavoib <joaogustavo831@gmail.com>
2023-11-10 12:55:51 -0300

    Procedimentos enviados para testes

--- Procedimentos.py
+++ Procedimentos.py
@@ -6,33 +6,31 @@ conn = psycopg2.connect(host="200.129.44.249", database="537063", user="537063",
 # Criando um cursor
 cur = conn.cursor()
 
-cur.execute("""CREATE OR REPLACE PROCEDURE empregado_do_mes(data_parametro DATE)
-LANGUAGE plpgsql
-AS $$
-DECLARE
-    max_movimentacoes int := 0;
-    empregado_id int;
-    empregado_nome text;
-	empregado_rec empregados%rowtype;
-BEGIN
-    FOR empregado_rec IN
-        SELECT e.id_emp, e.nome, COUNT(m.id_mov) AS num_movimentacoes
-        FROM (empregados e INNER JOIN movimentacao_empregados me ON me.id_emp = e.id_emp) INNER JOIN movimentacao m ON m.id_mov = me.id_mov
-        WHERE EXTRACT(YEAR FROM m.data) = EXTRACT(YEAR FROM data_parametro)
-          AND EXTRACT(MONTH FROM m.data) = EXTRACT(MONTH FROM data_parametro)
-        GROUP BY e.id_emp, e.nome
-    LOOP
-        IF empregado_rec.num_movimentacoes > max_movimentacoes THEN
-            max_movimentacoes := empregado_rec.num_movimentacoes;
-            empregado_id := empregado_rec.id_emp;
-            empregado_nome := empregado_rec.nome;
-        END IF;
-    END LOOP;
-
-    -- Imprime o resultado na saída
-    RAISE NOTICE 'Empregado do mês: ID= %, Nome= %', empregado_id, empregado_nome;
-END;
-$$;""")
+# Criando o procedimento que deve realizar 1. Crie um procedimento armazenado no banco de dados com nome empregado do mes que recebe como
+# parâmetro uma data e retorna o id e nome do empregado que participou de mais movimentações naquela
+# combinação Ano/Mês;
+cur.execute("""
+            CREATE OR REPLACE PROCEDURE empregado_do_mes(IN data_parametro DATE)
+            LANGUAGE plpgsql
+            AS $$
+            DECLARE
+                empregado_id INTEGER;
+                empregado_nome VARCHAR(50);
+            BEGIN
+                -- Seleciona o empregado que participou de mais movimentações naquele mês
+                SELECT e.id_emp, e.nome INTO empregado_id, empregado_nome
+                FROM Empregados e INNER JOIN Movimentacao_Empregados me ON e.id_emp = me.id_emp
+                INNER JOIN Movimentacao m ON me.id_mov=m.id_mov
+                WHERE EXTRACT(MONTH FROM m.data) = EXTRACT(MONTH FROM data_parametro) AND EXTRACT(YEAR FROM m.data) = EXTRACT(YEAR FROM data_parametro)
+                GROUP BY e.id_emp, e.nome
+                ORDER BY COUNT(*) DESC;
+            
+                -- Retorna o id e nome do empregado
+                RAISE NOTICE 'Empregado do mês: %, %', empregado_id, empregado_nome;
+                            
+            END;
+            $$;
+""")
 
 # Commitar as inserções
 conn.commit()
--- Teste_Procedimento.py
+++ Teste_Procedimento.py
@@ -6,7 +6,7 @@ conn = psycopg2.connect(host="200.129.44.249", database="537063", user="537063",
 # Criando um cursor
 cur = conn.cursor()
 
-cur.execute("CALL empregado_do_mes('2023-11-09');")
+cur.execute("CALL empregado_do_mes('2023-10-01');")
 
 cur.close()
 
